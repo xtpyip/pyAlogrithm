80779a9b2c77597ef8e24858d432c04ddc739700
pyip <pyip2001@qq.com>
2022-07-23 18:23:21 +0800

    'chapter1'

--- chapter1/Problem_03_ReverseStackUsingRecursive.py
+++ chapter1/Problem_03_ReverseStackUsingRecursive.py
@@ -9,6 +9,7 @@ class ReverseStack(object):
         self.stack.push(3)
         self.stack.push(4)
 
+    # 此函数每次返回并删除最底部的数字，保持上方相对顺序不变
     def getAndRemoveElement(self):
         result = self.stack.pop()
         if self.stack.is_empty():
@@ -18,10 +19,12 @@ class ReverseStack(object):
             self.stack.push(result)
             return last
 
+    # 此函数逆转栈
     def recursive(self):
         if self.stack.is_empty:
             return
         value = self.getAndRemoveElement(self.stack)
+        # 从底部拿出数字，并从最后的递归函数依次压入栈中
         self.recursive(self.stack)
         self.stack.push(value)
 
